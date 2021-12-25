eb58987c5e36f8d3f60b4e785bdf065c070239e6
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-12-25 18:53:30 +0530

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -4,4 +4,6 @@ en=input("\033[38;5;22;48;5;227m python payload or virus file name enter ->> ")
 
 
 with open(en,'r') as f:
-    print(eval(f'binascii.hexlify(b"{f.read()}")'))
+    t=eval(f'binascii.hexlify(b"{f.read()}")')
+    o=open('main.txt','w')
+    o.write(t)
