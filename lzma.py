092aed964af9de3e40f1f7b7b551b7b0037c77e5
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-12-28 17:57:24 +0530

    Update lzma.py

--- lzma.py
+++ lzma.py
@@ -4,7 +4,7 @@ i=input('input file')
 o=open(i,'r')
 
 k=lzma.LZMACompressor()
-k=k.compress(o.read)+k.flush()
+k=k.compress(o.read())+k.flush()
 o=open(input('enter output file name'),'w')
 
 o.write(k)
